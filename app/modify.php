254a30e855828aa1c607933dc97d110b226943a3
Jorge <jillera10@gmail.com>
2024-09-19 16:32:28 +0200

    modify

--- app/modify.php
+++ app/modify.php
@@ -2,4 +2,13 @@
     session_start(); // Iniciar una sesión para poder almacenar datos de usuario
     require_once 'db_connection.php'; // Conexión a la base de datos
     include('modify.html'); // Incluir el formulario de modificación de datos
+
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
+        echo "ID: $id";
+        
+    }
+    else{
+        echo "No se ha recibido el ID del usuario.";
+    }
 ?>
