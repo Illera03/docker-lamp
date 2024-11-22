c5952807281d3fd709e1e3af66ca5f1e9493cde7
Jorge <jillera10@gmail.com>
2024-11-22 16:13:03 +0100

    configuracíon de seguridad insuficiente

--- app/db_connection.php
+++ app/db_connection.php
@@ -1,7 +1,7 @@
 <?php
     $hostname = "db";
     $username = "admin__sgssi";
-    $password = "u31sg0909";
+    $password = "U31sg09_09";
     $db = "database";
 
     // Conexión a la base de datos
